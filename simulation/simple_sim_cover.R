92e7ff4e3b2c720745c0882bb4bae71af6396f31
Spencer Wadsworth <wadsworth.spencer.g@gmail.com>
2025-12-20 20:19:15 -0500

    add alternative horseshoe prior

--- simulation/simple_sim_cover.R
+++ simulation/simple_sim_cover.R
@@ -38,11 +38,16 @@ mod_name <- ifelse(model == "clt_shs",
                    "cdf_quantile_normal_mixK_shs.stan",
                    ifelse(model == "clt", "cdf_quantile_normal_mixK.stan",
                           ifelse(model == "ord", "order_normal_mixK_quantiles.stan",
-                                 ifelse(model == "ord_shs",
-                                        "order_normal_mixK_quantiles_shs.stan",
-                                        ifelse(model == "ind_shs",
-                                               "cdf_ind_quantile_normal_mixK_shs.stan",
-                                               "cdf_ind_quantile_normal_mixK.stan")))))
+                                 ifelse(model == "clt_hs", 
+                                        "cdf_quantile_normal_mixK_hs.stan",
+                                        ifelse(model == "ord_hs",
+                                               "order_normal_mixK_quantiles_hs.stan",
+                                               
+                                               ifelse(model == "ord_shs",
+                                                      "order_normal_mixK_quantiles_shs.stan",
+                                                      ifelse(model == "ind_shs",
+                                                             "cdf_ind_quantile_normal_mixK_shs.stan",
+                                                             "cdf_ind_quantile_normal_mixK.stan")))))))
 
 mod <- cmdstan_model(stan_file = paste0(mod_loc, mod_name))
 
