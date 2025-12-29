9cd5991fa6157281538195cd75d4821ab59c29cc
iac25002 <iac25002@login5.storrs.hpc.uconn.edu>
2025-12-29 12:00:04 -0500

    do old commit

--- simulation/simple_sim.sh
+++ simulation/simple_sim.sh
@@ -12,4 +12,4 @@
 module purge
 module load gsl/2.8 udunits/2.2.28-gcc14.2 cuda/11.6 freetype/2.12.1 gdal/3.9.2 r/4.4.2 proj geos cmake
 #
-Rscript simple_sim_cover.R "$1" "$2" "$3" "$4"
+Rscript simple_sim_cover.R "$1" "$2" "$3" "$4" "$5"
